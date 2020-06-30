7fe3cac80f64b0b94b9aa627aa439f64685ec379
Danica Fisher <dlwylin@gmail.com>
2020-06-29 17:05:13 -0700

    Updated dependencies

--- server/start.js
+++ server/start.js
@@ -1,8 +1,7 @@
 const app = require('./index.js');
-const db = require('../database/index.js');
 const port = 3000;
 
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}/:id`);
+  console.log(`Proxy server running at: http://localhost:${port}/:id`);
 });
